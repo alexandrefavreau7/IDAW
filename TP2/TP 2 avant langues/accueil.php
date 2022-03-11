0a4f94d28c10971da70f6b8949ccec6277ebf501
alexandrefavreau7 <101090637+alexandrefavreau7@users.noreply.github.com>
2022-03-11 10:45:48 +0100

    TP 2 Avant langues

---
+++ TP2/TP 2 avant langues/accueil.php
@@ -0,0 +1,4 @@
+
+    
+<h1>Bienvenue sur le site crée en IDAW</h1>
+
---
+++ TP2/TP 2 avant langues/hobbies.php
@@ -0,0 +1,17 @@
+
+
+    <p>Football</p> 
+      <figure class="ballon"> 
+        <img src="images/ballon.png" width="100" height="100">
+
+      <p>Montagne</p> 
+      <figure class="montagne"> 
+        <img src="images/montagne.png" >
+
+
+
+
+
+  </figure>
+
+ 
